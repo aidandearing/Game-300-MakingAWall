ccc861c4c202e07e39d765afbdebd377d6de0cb6
aidandearing <aidan.dearing@live.com>
2015-10-09 19:50:36 -0400

    Published the project
    Builds a big cube of cubes that can be shot with a gun, turned off
    gravity.

---
+++ Assets/Shoot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+    public float speed = 20;
+    public Rigidbody proj;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    if (Input.GetButton("Fire1"))
+        {
+            Rigidbody instantiateProjectile = Instantiate(proj, transform.position, transform.rotation) as Rigidbody;
+            instantiateProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed));
+            Destroy(instantiateProjectile.gameObject, 10);
+            Debug.Log("Firing");
+        }
+	}
+}
---
+++ Assets/Wall.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour {
+    public Rigidbody wall;
+
+	// Use this for initialization
+	void Start () {
+        for (int x = 0; x < 8; x++)
+        {
+            for (int y = 0; y < 8; y++)
+            {
+                for (int z = 0; z < 8; z++)
+                {
+                    Rigidbody instantiateProjectile = Instantiate(wall, transform.position, transform.rotation) as Rigidbody;
+                    instantiateProjectile.position = transform.TransformDirection(new Vector3(10 + x * 10f, 10 + y * 10f, 10 + z * 10f));
+                }
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
